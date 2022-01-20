8d979d2104a997335c41f1c6f6a6fdb5580f9534
Maciej <macko939@gmail.com>
2022-01-20 15:36:50 +0000

    Update myinstances.sh

--- myinstances.sh
+++ myinstances.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Use AWS CLI backend to obtain the required information 
 aws ec2 describe-instances --filters Name=tag-key,Values=Name --query 'Reservations[*].Instances[*].{Instance:InstanceId,IP:PublicIpAddress,Name:Tags[?Key==`Name`]|[0].Value,Note:Tags[?Key==`Note`]|[0].Value}' --output table
